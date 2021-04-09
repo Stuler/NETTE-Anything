6a05ce357ca0b313ef60c18d0b83589e1f33bc4e
Stuler <peter.jurek@gmail.com>
2021-04-09 09:11:07 +0200

    clientList component ajaxed

--- nette-blog/app/Components/ClientList/ClientList.php
+++ nette-blog/app/Components/ClientList/ClientList.php
@@ -5,11 +5,19 @@ namespace App\Components\ClientList;
 
 use Nette\Application\UI\Control;
 use Nette\Database\Explorer;
+use App\Models\ProcessManagers\ClientsProcessManager;
+use App\Models\Repository\ClientsRepository;
 
 class ClientList extends Control {
 
 	/** @var Explorer @inject @internal */
 	public $db;
+
+    /** @var ClientsProcessManager @inject @internal */
+    public $clientsPM;
+
+    /** @var ClientsRepository @inject @internal */
+    public $clientsRepo;
 
 	private $count = null;
 
@@ -18,17 +26,15 @@ class ClientList extends Control {
 	}
 
 	public function render() {
-		$query = $this->db->table("client");
-		if ($this->count) {
-			$query->limit($this->count);
-		}
-		$this->template->clients = $query->fetchAll();
+		$clients = $this->clientsRepo->fetchAllActive();
+		$this->template->clients = $clients;
 
 		$this->template->setFile(__DIR__ . "/clientList.latte");
 		$this->template->render();
 	}
 
 	public function handleDelete(int $id) {
-		$this->db->table("client")->where("id", $id)->delete();
+        $this->clientsPM->removeClient($id);
+        $this->redrawControl("list");
 	}
 }
