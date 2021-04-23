6a08f1053baf2e8d9757e0ee290a0f5502519114
Stuler <peter.jurek@gmail.com>
2021-04-23 15:58:16 +0200

    todo: nova uloha

--- nette-blog/app/Components/ClientDetail/ClientDetail.php
+++ nette-blog/app/Components/ClientDetail/ClientDetail.php
@@ -9,17 +9,18 @@ use App\Components\FileSystem\FileSystem;
 use App\Components\FileSystem\FileSystemFactory;
 use Nette\Application\UI\Control;
 use Nette\Application\UI\Form;
+
 //use Nette\Database\Explorer;
 use App\Models\ProcessManagers\ClientsProcessManager;
 use App\Models\Repository\ClientsRepository;
 
 class ClientDetail extends Control
 {
-	/** @var ClientsProcessManager @inject @internal */
-	public $clientsPM;
+    /** @var ClientsProcessManager @inject @internal */
+    public $clientsPM;
 
-	/** @var ClientsRepository @inject @internal */
-	public $clientsRepo;
+    /** @var ClientsRepository @inject @internal */
+    public $clientsRepo;
 
     /** @var FileSystemFactory @inject @internal */
     public $fileSystemFactory;
@@ -37,9 +38,10 @@ class ClientDetail extends Control
 
 //   perzistentny parameter $id ziskavam z clientList handleShowModal
 
-    public function render() {
+    public function render()
+    {
 
-    	$id = (int)$this->id;
+        $id = (int)$this->id;
         if ($id) {
             $client = $this->clientsRepo->fetchById($id);
             $this['clientForm']->setDefaults($client);
@@ -47,106 +49,106 @@ class ClientDetail extends Control
             $client_person = $this->clientsRepo->fetchContactById($id);
             $this->template->contacts = $client_person;
 
-        } else{
-		$this->template->contacts = [];
-		}
+        } else {
+            $this->template->contacts = [];
+        }
         $this->template->isEdit = $id != null; //
 
         $this->template->setFile(__DIR__ . "/clientDetail.latte");
         $this->template->render();
     }
 
-	public function createComponentClientForm(): form
-	{
-		$form = new Form();
+    public function createComponentClientForm(): Form
+    {
+        $form = new Form();
 
-		$form->getElementPrototype()->class("ajax");
+        $form->getElementPrototype()->class("ajax");
 
-		$form->addHidden("id");
+        $form->addHidden("id");
 
-		$form->addText("name", "Client name")
-			->addRule(Form::FILLED, "Enter client name");
+        $form->addText("name", "Client name")
+            ->addRule(Form::FILLED, "Enter client name");
 
-		$form->addText("ico", "Client ICO");
+        $form->addText("ico", "Client ICO");
 
-		$form->addText("dic", "Client DIC");
+        $form->addText("dic", "Client DIC");
 
-		$form->addText("number", "Client number");
+        $form->addText("number", "Client number");
 
-		$form->addText("street_num", "Street number");
+        $form->addText("street_num", "Street number");
 
-		$form->addText("city", "City");
+        $form->addText("city", "City");
 
-		$form->addText("zip", "ZIP Code");
+        $form->addText("zip", "ZIP Code");
 
-		$form->addText("email", "Client's email address");
+        $form->addText("email", "Client's email address");
 
-		$form->addText("mobile", "Client cell phone number");
+        $form->addText("mobile", "Client cell phone number");
 
-		$form->addText("phone", "Client telephone number");
+        $form->addText("phone", "Client telephone number");
 
-		$form->addText("fax", "Client fax number");
+        $form->addText("fax", "Client fax number");
 
-		$form->addText("website", "Client website");
+        $form->addText("website", "Client website");
 
-		$form->addText("contact_person", "Contact person");
+        $form->addText("contact_person", "Contact person");
 
-		$form->addText("note", "Note");
+        $form->addText("note", "Note");
 
-		$form->addSubmit("send", "Založit");
+        $form->addSubmit("send", "Založit");
 
-		$form->onSuccess[] = function (Form $form) {
+        $form->onSuccess[] = function (Form $form) {
 
-			$values = $form->getValues();
-			$data = (array)$values;
-			if ($values['id']) {
-				$this->clientsPM->updateClient((int)$values['id'], (array)$data);
-				$form->setValues($values, true);
+            $values = $form->getValues();
+            $data = (array)$values;
+            if ($values['id']) {
+                $this->clientsPM->updateClient((int)$values['id'], (array)$data);
+                $form->setValues($values, true);
                 $this->redrawControl("form");
 //				$this->redrawControl("clientForm");
 //				$this->redrawControl("clientList");
-			} else {
-				$this->clientsPM->addClient($data);
-				$id = $this->clientsRepo->db->getInsertId();
-				$this->id = $id;
-				$form->setDefaults([$values['id']=>$id]);
-				$this->redrawControl("form");
+            } else {
+                $this->clientsPM->addClient($data);
+                $id = $this->clientsRepo->db->getInsertId();
+                $this->id = $id;
+                $form->setDefaults([$values['id'] => $id]);
+                $this->redrawControl("form");
 
 //              $this->redrawControl("contactForm");
 //				$this->redrawControl("contactList");
 //				$values['id']=$this->id;
 //				$this->redirect("this", ["id" => $id]);
-			}
+            }
 
-			//nastavi id do formulara a nevymaze jeho hodnoty - mozem editovat klienta
-			$form->setValues(['id'=>$this->id], false);
-			bdump($this->id);
-		};
-		return $form;
-	}
+            //nastavi id do formulara a nevymaze jeho hodnoty - mozem editovat klienta
+            $form->setValues(['id' => $this->id], false);
+            $this->onChange(); // potvrdenim naslucham udalosti onChange - je nasledne vyvolana v clientsPresenter
+        };
+        return $form;
+    }
 
-	public function createComponentPersonForm(): Form
-	{
-		$form = new Form();
+    public function createComponentPersonForm(): Form
+    {
+        $form = new Form();
 
-		$form->getElementPrototype()->class("ajax");
+        $form->getElementPrototype()->class("ajax");
 
-		$form->addHidden("id");
+        $form->addHidden("id");
 
         $form->addHidden("client_id")
-			->setDefaultValue($this->id);
+            ->setDefaultValue($this->id);
 
-		$form->addText("name", "Meno")
-			->addRule(Form::FILLED, "Uveď meno kontaktnej osoby!");
+        $form->addText("name", "Meno")
+            ->addRule(Form::FILLED, "Uveď meno kontaktnej osoby!");
 
-		$form->addText("email", "Email")
-			->addRule(Form::FILLED, "Uveď emailovú adresu kontaktnej osoby!");
+        $form->addText("email", "Email")
+            ->addRule(Form::FILLED, "Uveď emailovú adresu kontaktnej osoby!");
 
-		$form->addText("phone", "Telefón");
+        $form->addText("phone", "Telefón");
 
-		$form->addText("status", "Pozícia");
+        $form->addText("status", "Pozícia");
 
-		$form->addSubmit("send", "Pridať kontaktnú osobu");
+        $form->addSubmit("send", "Pridať kontaktnú osobu");
 
         $form->onSuccess[] = function (Form $form, $values) {
             $data = (array)$values;
@@ -164,36 +166,36 @@ class ClientDetail extends Control
         return $form;
     }
 
-	public function createComponentFileSystem(): FileSystem
-	{
-		$fileSystem = $this->fileSystemFactory->create();
-		$fileSystem->clientId = $this->id;
-		return $fileSystem;
-	}
+    public function createComponentFileSystem(): FileSystem
+    {
+        $fileSystem = $this->fileSystemFactory->create();
+        $fileSystem->clientId = $this->id;
+        return $fileSystem;
+    }
 
-	// public function createComponentClientList(): ClientList {
-	// 	$clientList = $this->clientListFactory->create();
-	// 	return $clientList;
-	// }
+    // public function createComponentClientList(): ClientList {
+    // 	$clientList = $this->clientListFactory->create();
+    // 	return $clientList;
+    // }
 
-	public function handleEditPerson(int $contactId)
-	{
-		$values = $this->clientsRepo->fetchContact($contactId);
-		$this['personForm']->setDefaults($values);
-		$this->redrawControl("contactForm");
-	}
+    public function handleEditPerson(int $contactId)
+    {
+        $values = $this->clientsRepo->fetchContact($contactId);
+        $this['personForm']->setDefaults($values);
+        $this->redrawControl("contactForm");
+    }
 
-	public function handleDeleteContact(int $contactId)
-	{
-		$this->clientsPM->removeContact($contactId);
-		$this->redrawControl("contactList");
-	}
+    public function handleDeleteContact(int $contactId)
+    {
+        $this->clientsPM->removeContact($contactId);
+        $this->redrawControl("contactList");
+    }
 
-	public function handleShowModal()
-	{
-		$this->template->showModal = true;
-		$this->redrawControl("modal");
-	}
+    public function handleShowModal()
+    {
+        $this->template->showModal = true;
+        $this->redrawControl("modal");
+    }
 
     public function handleCloseModal()
     {
--- nette-blog/readme.md
+++ nette-blog/readme.md
@@ -1,46 +1,15 @@
-Nette Web Project
+Úkoly na DB
 =================
 
-This is a simple, skeleton application using the [Nette](https://nette.org). This is meant to
-be used as a starting point for your new projects.
+- Výpsat jedním dotazem 3 sloupce. V prvním bude id client.id. V druhém bude název client_person.name a v třetím bude
+  client.name
+  SELECT client.id, client_person.name, client.name FROM client INNER JOIN client_person ON client.id = client_person.client_id
 
-[Nette](https://nette.org) is a popular tool for PHP web development.
-It is designed to be the most usable and friendliest as possible. It focuses
-on security and performance and is definitely one of the safest PHP frameworks.
+- Stejný výpis jako úkol nad ale vypsat jen firmy, které obsahují v názvu řetězec `any` To znamená že to musí najít
+  firmu `anything` ale i firmu `many`. Bez fulltext.
+  SELECT client.id, client_person.name, client.name FROM client INNER JOIN client_person ON client.id = client_person.client_id WHERE client.name LIKE '%any%'
 
-If you like Nette, **[please make a donation now](https://nette.org/donate)**. Thank you!
-
-
-Requirements
-------------
-
-- Web Project for Nette 3.1 requires PHP 7.2
+- Vypsat jedním dotazem pouze ty clienty kteří mají aspoň dva client_person.
+  SELECT client.id, client.name, (SELECT COUNT(*) FROM client_person WHERE client.id = client_person.client_id) as contact_count FROM client HAVING contact_count > 1
 
 
-Installation
-------------
-
-The best way to install Web Project is using Composer. If you don't have Composer yet,
-download it following [the instructions](https://doc.nette.org/composer). Then use command:
-
-	composer create-project nette/web-project path/to/install
-	cd path/to/install
-
-
-Make directories `temp/` and `log/` writable.
-
-
-Web Server Setup
-----------------
-
-The simplest way to get started is to start the built-in PHP server in the root directory of your project:
-
-	php -S localhost:8000 -t www
-
-Then visit `http://localhost:8000` in your browser to see the welcome page.
-
-For Apache or Nginx, setup a virtual host to point to the `www/` directory of the project and you
-should be ready to go.
-
-**It is CRITICAL that whole `app/`, `config/`, `log/` and `temp/` directories are not accessible directly
-via a web browser. See [security warning](https://nette.org/security-warning).**
