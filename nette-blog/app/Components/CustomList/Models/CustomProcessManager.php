18530627aa40e4b8e340c4ef631f1003cc635cff
Stuler <peter.jurek@gmail.com>
2021-05-03 09:25:38 +0200

    components models init

---
+++ nette-blog/app/Components/CustomList/Models/CustomProcessManager.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Components\CustomList\Models;
+
+use App\Components\CustomList\CustomList\CustomRepository;
+
+class CustomProcessManager
+{
+    
+}
---
+++ nette-blog/app/Components/CustomList/Models/CustomRepository.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Components\CustomList\Models;
+
+use Nette\Database\Context;
+use Nette\Database\Explorer;
+use Nette\Database\Row;
+
+class CustomRepository
+{
+
+}
